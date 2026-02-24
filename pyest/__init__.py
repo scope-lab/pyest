17c09e24f7f3f8d00d27610f2ca61ad6e30f73dd
Keith LeGrand <26415081+keithalegrand@users.noreply.github.com>
2026-02-24 18:49:30 -0500

    resolved dependency on deprecated pkg_resources

--- pyest/__init__.py
+++ pyest/__init__.py
@@ -1,11 +1,4 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
-
+from pyest._version import __version__
 import pyest.gm
 import pyest.filters
 import pyest.sensors
